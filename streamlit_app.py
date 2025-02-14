f31bd0b6b59bbf4dced49673b9222f34bd49b10f
thierrycosendai <thierrycosendai@gmail.com>
2025-02-14 12:39:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-
-st.write("hello world");
-st.title('Mon titre')
+def main()
+user_input = st.text_put ("Entrer la longueur du sepale:")
+user_input = st.text_put("Entrer la largeur du sepale:")
+user_input = st.text_put("Entrer la longueur du petale:")
+user_input = st.text_put("Entrer la largeur du petale:")
+st.button( 'Soumettre')
+if st.button("Soumettre")
+st.write(fLa variété de la fleur:"{user_input}!)
+#st.write("hello world");
+#st.title('Mon titre')
 st.button('Submit')
