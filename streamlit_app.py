bd263460ab062cf77a62956456958ca61bc10556
thierrycosendai <thierrycosendai@gmail.com>
2025-02-14 12:42:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ user_input = st.text_put("Entrer la longueur du petale:")
 user_input = st.text_put("Entrer la largeur du petale:")
 st.button( 'Soumettre')
 if st.button("Soumettre")
-st.write(fLa variété de la fleur:"{user_input}!)
+#st.write(fLa variété de la fleur:"{user_input}!)
 #st.write("hello world");
 #st.title('Mon titre')
 st.button('Submit')
