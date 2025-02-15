3b7c170ea7ca8fa9b0a593a82ed73aa3d482c71f
thierrycosendai <thierrycosendai@gmail.com>
2025-02-15 14:02:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ user_input=st.text_input("Entrer la largeur du sepale:")
 user_input= st.text_input("Entrer la longueur du sepale:")
 if st.button("Soumettre")
 st.write(fLa variété de la fleur:"{user_input}!)
-main()
 st.write(fLa variété de la fleur:"{user_input}!)
 #st.write("hello world");
 #st.title('Mon titre')
