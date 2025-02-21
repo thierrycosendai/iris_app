3d5ac4e39a746add3487d471c065b53ee4ed667c
thierrycosendai <thierrycosendai@gmail.com>
2025-02-21 15:52:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ PetalWidth =st.slider("PetalWidth ", 0.0, 10.0, value=2.0, step=0.1)
 SepalWidth = st.slider("SepalWidth", 0.0, 10.0, value=0.3, step=0.1)
       
 if st.button("Prediction sur la fleur", help="Cliquez pour envoyer les donnees", type="primary"): 
-    model= joblib.load("mon_model2.pkl")
-    scaler= joblib.load("mon_scaler.pkl")
-    
+   
+   
     f = np.array([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
     X = pd.DataFrame(f,columns=["SepalLength", "SepalWidth", "PetalLength", "PetalWidth"])
-    
+    scaler= joblib.load("mon_scaler.pkl")
+    model=joblib.load("mon_model2.pkl)
     X_transform = scaler.transform(X)
     prediction = model.predict(X_transform)
     response = prediction[0]
